dc37d7cb073b74bef1d08db521672c68e7ccb123
Kagami <duchien230904@gmail.com>
2023-07-13 12:11:54 +0700

    Design 404 page

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-import express from "express"
-import configViewEngine from "./config/viewEngine.js"
-import 'dotenv/config'
-import initWebRoutes from "./routes/web.js" 
-import initAPIRoutes from "./routes/api.js"
+import express from "express";
+import morgan from "morgan";
+import configViewEngine from "./config/viewEngine.js";
+import "dotenv/config";
+import initWebRoutes from "./routes/web.js";
+import initAPIRoutes from "./routes/api.js";
 // import connection from "./config/connectDB.js"
 
 const app = express();
@@ -11,9 +12,25 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-configViewEngine(app)
-initWebRoutes(app)
-initAPIRoutes(app)
+app.use(morgan("combined"));
+
+// Setup view engine
+configViewEngine(app);
+
+// Initialize routes
+initWebRoutes(app);
+
+// Initialize routes for API
+initAPIRoutes(app);
+
+// Handle 404 not found errors
+app.use((req, res) => {
+  return res.render("404.ejs");
+});
+
+app.use((req, res) => {
+  
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
